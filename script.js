e65e4d58fed6cd6c0b2d0aec307164915ed8de6d
Faizan Shaikh <fshaikh412544@gmail.com>
2025-12-23 15:12:42 +0530

    Completed responsive fronted pages and update styles and scripts

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const shopNowBtn=document.getElementById("shopNowBtn");
+if(shopNowBtn){
+    shopNowBtn.addEventListener("click",() =>{
+        window.location.href="shop.html";
+    });
+}
+
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close= document.getElementById('close');
@@ -12,3 +19,41 @@ if(close){
         nav.classList.remove('active')
     })
 }  
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cartButtons = document.querySelectorAll(".cart-bnt");
+
+    cartButtons.forEach(bnt => {
+        bnt.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation(); // prevents redirect
+
+            const product = {
+                id: bnt.dataset.id,
+                name: bnt.dataset.name,
+                price: bnt.dataset.price,
+                img: bnt.dataset.img,
+                quantity: 1
+            };
+
+            addToCart(product);
+            alert("Added to cart 🛒");
+        });
+    });
+});
+
+function addToCart(product) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existing = cart.find(item => item.id === product.id);
+
+    if (existing) {
+        existing.quantity += 1;
+    } else {
+        cart.push(product);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
